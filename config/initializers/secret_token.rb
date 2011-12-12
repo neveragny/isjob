d6a0e32543c11957eaa0ca577776afe3b72b1b0a
neveragny <neveragny@gmail.com>
2011-12-12 04:27:58 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Getajob::Application.config.secret_token = '9a12c1063e70a65ccea2a637e8a15914ef28bbe9b519cd53990baca249d1359612d71acbd8e71777d89ad30c60bf1eb2d01b7feedda865eb3ea252d3f25b0585'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Getajob::Application.config.session_store :cookie_store, key: '_getajob_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Getajob::Application.config.session_store :active_record_store
