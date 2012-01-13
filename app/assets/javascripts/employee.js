01bf069335af8e3a92f03084b5c1ad814f325268
Nikita Demeshchenko <neveragny@gmail.com>
2012-01-13 23:17:07 +0200

    forms validations

--- app/assets/javascripts/employee.js
+++
@@ -1,8 +0,0 @@
-clientSideValidations.callbacks.element.fail = function(element, message, callback) {
-  callback();
-  if (element.data('valid') !== false) {
-    //element.parent().find('.message').hide().show('slide', {direction: "left", easing: "easeOutBounce"}, 500);
-    element.addClass("error");
-    element.parent().parent().addClass("error");
-  }
-}
